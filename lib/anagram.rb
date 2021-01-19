9633ed465ea95089c3830c5a6d1b44a150033237
nbyrd9 <nianibyrd@gmail.com>
2021-01-19 15:54:59 -0500

    commit tree 92600b4c5b73671bca8f293da6bee8233036df83

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+attr_accessor :word
+    def initialize(word)
+        @word= word
+    end 
+
+    def match(word_array)
+        word_array.select do |words|
+          words.split("").sort == self.word.split("").sort
+        end
+    end
+end
